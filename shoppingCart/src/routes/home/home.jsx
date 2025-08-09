1395c7564f9f1cce0e630afdedb39519af24b144
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-08-09 15:53:06 +0100

    add some styling to home page

--- shoppingCart/src/routes/home/home.jsx
+++ shoppingCart/src/routes/home/home.jsx
@@ -1,9 +1,15 @@
 import styles from './home.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
-      <div className={styles.home}>this is home!</div>
+      <div className={styles.home}>
+        <h2 className={styles.homeText}>The future of shopping</h2>
+        <Link to="store" className={styles.shopNowBtn}>
+          SHOP NOW
+        </Link>
+      </div>
     </>
   );
 }

83a2d70a51da30a6bbe9be458ac955c83657d6b1
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-08-09 14:25:31 +0100

    style navbar

--- shoppingCart/src/routes/root/root.jsx
+++ shoppingCart/src/routes/root/root.jsx
@@ -7,8 +7,14 @@ export default function Root() {
     <>
       <div className={styles.navBar}>
         <h1>Miles' Store</h1>
-        <button className={styles.navBtn}>Home</button>
-        <button className={styles.navBtn}>Store</button>
+        <div className={styles.navBtns}>
+          <button className={styles.navBtn}>
+            Home <hr color="black" />
+          </button>
+          <button className={styles.navBtn}>
+            Store <hr color="black" />
+          </button>
+        </div>
         <button className={styles.iconBtn}>
           <ShoppingCart />
         </button>

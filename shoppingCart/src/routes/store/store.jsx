1376f4f203d5a1ad1eab79e719a482dbf02b9e02
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-08-10 20:59:50 +0100

    add working fakeditemapi cards

--- shoppingCart/src/routes/store/store.jsx
+++ shoppingCart/src/routes/store/store.jsx
@@ -1,9 +1,37 @@
 import styles from './store.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Store() {
+  const [items, setItems] = useState();
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
     <>
-      <div className={styles.store}>this is store!</div>
+      <div className={styles.store}>
+        {items.map((item) => {
+          return (
+            <ItemCard title={item.title} img={item.image} price={item.price} />
+          );
+        })}
+      </div>
     </>
+  );
+}
+
+function ItemCard({ title, img, price }) {
+  return (
+    <div className={styles.card}>
+      <img src={img} alt="" />
+      <h2 className={styles.itemName}>{title}</h2>
+      <div className={styles.itemPrice}>{price}</div>
+      <div className={styles.quantity}>
+        <label htmlFor="quantity">Quantity:</label>
+        <input type="number" id={styles.quantity} />
+      </div>
+      <button className={styles.addToCartBtn}>Add to Cart</button>
+    </div>
   );
 }
